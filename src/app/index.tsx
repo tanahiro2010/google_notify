281608554e6af4e482fef998b1511ddfb424d732
tanahiro2010 <herentongkegu087@gmail.com>
2026-06-16 07:44:08 +0900

    feat: プロフィール情報の取得を追加し、表示を更新

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,19 @@
+import { useEffect, useState } from "react";
+
 const IndexPage = () => {
+  const [profile, setProfile] = useState<any>(null);
+
+  useEffect(() => {
+    setInterval(() => {
+      setProfile(sessionStorage.getItem("profile"));  
+    }, 1000);
+  }, []);
 
   return (
     <div>
       <h2>hello</h2>
+      <div>{JSON.stringify(profile)}</div>
+      <div>{JSON.stringify(localStorage.getItem("session"))}</div>
     </div>
   )
 };
