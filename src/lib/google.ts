5d54c5fc969fdd3ab502294cc6b92184a14bdb07
tanahiro2010 <herentongkegu087@gmail.com>
2026-06-20 21:17:02 +0900

    feat: GoogleAPIClientのfetchメソッドでURLの構築を改善し、searchParamsを使用するように変更

--- src/lib/google.ts
+++ src/lib/google.ts
@@ -34,16 +34,22 @@ class GoogleAPIClient {
 
   // Google Classroom API
   async fetchCourses() {
-    const url = `https://classroom.googleapis.com/v1/courses?courseStates=ACTIVE&pageSize=50`;
-    const res = await this.fetch(url);
+    const url = new URL(`https://classroom.googleapis.com/v1/courses`);
+    url.searchParams.append("courseStates", "ACTIVE");
+    url.searchParams.append("pageSize", "50");
+    
+    const res = await this.fetch(url.toString());
     const data: ListCoursesResponse = await res.json();
 
     return data.courses ?? [];
   }
 
   async fetchCourseWorks(courseId: string) {
-    const url = `https://classroom.googleapis.com/v1/courses/${courseId}/courseWork?orderBy=updateTime desc&pageSize=20`;
-    const res = await this.fetch(url);
+    const url = new URL(`https://classroom.googleapis.com/v1/courses/${courseId}/courseWork`);
+    url.searchParams.append("orderBy", "updateTime desc");
+    url.searchParams.append("pageSize", "20");
+
+    const res = await this.fetch(url.toString());
     const data: ListCourseWorkResponse = await res.json();
 
     return data.courseWork ?? [];
@@ -51,16 +57,22 @@ class GoogleAPIClient {
 
   // Google Chat API
   async fetchChatSpaces() {
-    const url = `https://chat.googleapis.com/v1/spaces?pageSize=50`;
-    const res = await this.fetch(url);
+    const url = new URL(`https://chat.googleapis.com/v1/spaces`);
+    url.searchParams.append("pageSize", "50");
+
+    const res = await this.fetch(url.toString());
     const data: ListSpacesResponse = await res.json();
 
     return data.spaces ?? [];
   }
 
   async fetchChatMessages(spaceName: string, filter: string) {
-    const url = `https://chat.googleapis.com/v1/${spaceName}/messages?filter=${filter}&orderBy=createTime desc&pageSize=20`;
-    const res = await this.fetch(url);
+    const url = new URL(`https://chat.googleapis.com/v1/${spaceName}/messages`);
+    url.searchParams.append("filter", filter);
+    url.searchParams.append("orderBy", "createTime desc");
+    url.searchParams.append("pageSize", "20");
+    
+    const res = await this.fetch(url.toString());
     const data: ListMessagesResponse = await res.json();
 
     return data.messages ?? [];
