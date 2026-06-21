88081e527157b6037f3c367da984b6004012639a
tanahiro2010 <herentongkegu087@gmail.com>
2026-06-21 23:09:08 +0900

    feat: MUIのProviderとCssBaselineを追加

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import App from "./App";
+import { theme } from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
